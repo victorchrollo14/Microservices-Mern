ee86a298f62b9c4e8a362f4066edce56e869db24
victor <chrollolucilfer1402@gmail.com>
2023-08-22 13:41:08 +0530

    added header to all pages

--- client/src/pages/cart.jsx
+++ client/src/pages/cart.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../components/header";
 
 export const Cart = () => {
   return (
     <div>
+      <Header />
       <h1>cart page</h1>
     </div>
   );
--- client/src/pages/product.jsx
+++ client/src/pages/product.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../components/header";
 
 export const Product = () => {
   return (
     <div>
+      <Header />
       <h1>Product page</h1>
     </div>
   );
 };
-
